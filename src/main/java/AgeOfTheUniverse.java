639c11be96a369284b864b510f756d65e86ac07b
Ilya Danilov <esozap32@mail.ru>
2022-10-30 21:02:03 +0400

    AgeOfTheUniverse

---
+++ src/main/java/AgeOfTheUniverse.java
@@ -0,0 +1,9 @@
+public class AgeOfTheUniverse {
+    public static void main(String[] args) {
+        long ageOfTheUniverse = 13000000000L;
+        int theAgeOfHumanity = 100000;
+        long difference = (ageOfTheUniverse / theAgeOfHumanity);
+        System.out.println("Вселенная старше людей в " + difference + " лет");
+    }
+}
+
